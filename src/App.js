0fbf4fdeaf3a42d6c343a60c8f28abd8f6839b88
lethaLe <lethale@anniecannons.com>
2021-06-23 11:57:46 -0700

    Recipe Card App : Recent Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import RecipesContainer from "./components/RecipesContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./firebase/firestore";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="head">LethaLe's React Recipe Box</h2>
+      <p className="sub-head">
+        Create, edit, delete your recipes. Uses session storage so that page
+        refresh will keep stored data (but not after page close)
+      </p>
+      <RecipesContainer />
     </div>
   );
-}
+};
 
 export default App;
