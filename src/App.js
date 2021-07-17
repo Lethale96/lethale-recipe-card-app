c82d718f93320a6832038be796e695783969e128
lethaLe <lethale@anniecannons.com>
2021-07-17 02:35:39 -0700

    Up-to-date code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
 import RecipesContainer from "./components/RecipesContainer";
+import {
+  Accordion,
+  Button,
+  Card,
+  FormControl,
+  InputGroup,
+  Modal,
+} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "./firebase/firestore";
 import "./App.css";
@@ -7,11 +15,13 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <h2 className="head">LethaLe's React Recipe Box</h2>
-      <p className="sub-head">
-        Create, edit, delete your recipes. Uses session storage so that page
-        refresh will keep stored data (but not after page close)
-      </p>
+      <div className="header">
+        <h2 className="head">LethaLe's React Recipe Box</h2>
+        <p className="sub-head">
+          Create, edit, delete your recipes. Uses session storage so that page
+          refresh will keep stored data (but not after page close)
+        </p>
+      </div>
       <RecipesContainer />
     </div>
   );
